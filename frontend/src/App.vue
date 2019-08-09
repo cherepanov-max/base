edec804e615384912ad648753c936e19e25c5969
cherepanov-max <cherepanov@ku66.ru>
2019-08-09 21:22:51 +0500

    init

---
+++ frontend/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+	<v-app >
+		<div>
+			<v-toolbar>
+				<v-toolbar-items>
+					<v-btn text to="/">Home</v-btn>
+					<v-btn text to="/login">Login</v-btn>
+				</v-toolbar-items>
+			</v-toolbar>
+		</div>
+		<div>
+			<v-content>
+				<v-container fluid> 
+					<router-view></router-view>
+				</v-container>
+			</v-content>
+		</div>
+	</v-app>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+
+  },
+  data: () => ({
+
+  }),
+};
+</script>
---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,43 @@
+<template>
+<v-layout column align-center>
+	<v-layout wrap justify-space-around>
+		<h2 class="mb-2">Login Form</h2>
+	</v-layout>
+	<v-form class="login-form">
+	  <v-text-field
+		label="E-mail"
+		required
+	  ></v-text-field>
+
+	  <v-text-field
+		label="Password"
+		required
+	  ></v-text-field>
+	<v-layout wrap justify-space-between>
+		<v-btn
+		color="success"
+	
+		width='45%'
+	  >
+		Sign In
+	  </v-btn>
+
+	  <v-btn
+		color="warning"
+		width='45%'
+	  >
+		Sign Up
+	  </v-btn>
+	</v-layout>
+	  
+	</v-form>
+  </v-layout>
+</template>
+
+<style lang="stylus" scoped>
+	.login-form
+		min-width 300px
+		max-width 400px
+		width 20%
+</style>
+
